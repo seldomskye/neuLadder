26277a8466db7cb81b1b4bb99971c55e73663a0d
Sean Andrews <andrews.se@husky.neu.edu>
2015-02-24 22:15:07 -0500

    Adding usercommands menu, players can make active
    Need to move topbar into the defaultLayout though, because there will
    not exist a page I don't want it on.  Then I should be able to easily
    add return messages for things on the admin page

--- Handler/UserCommands.hs
+++ Handler/UserCommands.hs
@@ -1,14 +1,52 @@
+module Handler.UserCommands where
+
+import Import
+import Yesod.Form.Bootstrap3
+
 -- Mark Self Active
 -- FindMatch
 
 getUserR :: Handler Html
 getUserR = do
-  
+  getPlayers <- runDB $ do
+          players <- selectList [] [Asc PlayerTag]
+          return $ map (\x -> (playerTag x, playerTag x)) $ map entityVal players
+  (playerActive, enctype) <- generateFormPost $ renderDivs $ playerActiveForm getPlayers
+  defaultLayout $ do
+    setTitle "NEUMelee Admin"
+    $(widgetFile "topbar")
+    [whamlet|<form method=post action=@{UserR}#form enctype=#{enctype}>
+    ^{playerActive}
+    <button .btn .btn-default type="submit">
+      Set Active
+|] 
+
+postUserR :: Handler Html
+postUserR = do
+  getPlayers <- runDB $ do
+    players <- selectList [] [Asc PlayerTag]
+    return $ map (\x -> (playerTag x, playerTag x)) $ map entityVal players
+  ((result, playerActive), enctype) <- runFormPost $ renderDivs $ playerActiveForm getPlayers
+  case result of
+    FormSuccess playerTag -> do
+      madeActive <- makeActive playerTag
+      defaultLayout $ do
+        setTitle "NEUMelee Admin"
+        $(widgetFile "topbar")
+        [whamlet|<form method=post action=@{UserR}#form enctype=#{enctype}>
+                            ^{playerActive}
+                            <button .btn .btn-default type="submit">
+                                                           Set Active
+                                                          |]
+    _ -> defaultLayout $(widgetFile "topbar")
+playerActiveForm players = areq (selectFieldList players) (bfs ("Winner" :: Text)) Nothing
 
 makeActive tag = runDB $ do
   now <- liftIO getCurrentTime
-  updateWhere [UniqueTag .= tag] [PlayerLastActive .= now, PlayerCurrentlyActive .= Just True]
+  updateWhere [PlayerTag ==. tag] [PlayerLastActive =. Just now, PlayerCurrentlyActive =. Just True]
+  return True
 
 findValidMatches player = runDB $ do
-  let canPlay =  [PlayerCurrentlyActive .== (Just True), PlayerRanking .> playerRanking player] 
+  let canPlay =  [PlayerCurrentlyActive ==. (Just True), PlayerRanking >. playerRanking player] 
   playables <- selectList canPlay [Asc PlayerRanking]
+  return playables
